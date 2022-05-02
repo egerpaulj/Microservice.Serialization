8f13daecac0a4ad5369209398af3856da064a71c
Paul Eger <egerpaulj@protonmail.com>
2022-05-02 20:19:22 +0200

    Improve seriailzationx

--- IJsonConverterProvider.cs
+++ IJsonConverterProvider.cs
@@ -15,6 +15,7 @@
 //      along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 using System;
+using System.Text;
 using Newtonsoft.Json;
 
 
@@ -25,8 +26,10 @@ namespace Microservice.Serialization
     /// <summary/>
     public interface IJsonConverterProvider
     {
-         JsonConverter[] GetJsonConverters();
-         public const string DateTimeFormat = "hh:MM:dd:mm.ss.fff";
+        JsonConverter[] GetJsonConverters();
+        public const string DateTimeFormat = "hh:MM:dd:mm.ss.fff";
+
+        public static Encoding TextEncoding = Encoding.UTF8;
     }
 
     public class EmptyJsonConverterProvider : IJsonConverterProvider
@@ -37,33 +40,53 @@ namespace Microservice.Serialization
     public static class JsonExtensions
     {
         public static string Serialize(this IJsonConverterProvider converter, object data) => JsonConvert.SerializeObject(data, converter.GetJsonConverters());
-        
-        public static T Deserialize<T>(this IJsonConverterProvider converter, string valueStr) => (T) converter.DeserializeObject<T>(valueStr);
+
+        public static T Deserialize<T>(this IJsonConverterProvider converter, string valueStr) => (T)converter.DeserializeObject<T>(valueStr);
 
         private static object DeserializeObject<T>(this IJsonConverterProvider converter, string valueStr)
         {
+            if(string.IsNullOrEmpty(valueStr))
+                return default(T);
+
             var type = typeof(T);
 
             if (type.IsValueType)
             {
-                if (type == typeof(string))
+                if (type == typeof(string) || type == typeof(String))
                     return valueStr;
 
-                if (type == typeof(bool))
+                if (type == typeof(bool) || type == typeof(Boolean))
                     return bool.Parse(valueStr);
 
-                if (type == typeof(int))
+                if (type == typeof(int) || type == typeof(Int16) || type == typeof(Int32) || type == typeof(Int64))
                     return int.Parse(valueStr);
 
-                if (type == typeof(double))
+                if (type == typeof(double) || type == typeof(Double))
                     return double.Parse(valueStr);
 
                 if (type == typeof(float))
                     return float.Parse(valueStr);
 
+
+
+                if (type == typeof(byte[]))
+                    return IJsonConverterProvider.TextEncoding.GetBytes(valueStr);
+
                 if (type == typeof(DateTime))
                     return DateTime.Parse(valueStr.Trim('"'));
             }
+
+            if (type == typeof(String))
+                return valueStr;
+
+            if (type == typeof(Boolean))
+                return bool.Parse(valueStr);
+
+            if (type == typeof(Int16) || type == typeof(Int32) || type == typeof(Int64))
+                return int.Parse(valueStr);
+
+            if (type == typeof(Double))
+                return double.Parse(valueStr);
 
             return JsonConvert.DeserializeObject(valueStr, typeof(T), converter.GetJsonConverters());
         }
@@ -75,7 +98,14 @@ namespace Microservice.Serialization
             if (type.IsValueType)
             {
                 if (type == typeof(DateTime))
-                    return ((DateTime) data).ToString(IJsonConverterProvider.DateTimeFormat);
+                    return ((DateTime)data).ToString(IJsonConverterProvider.DateTimeFormat);
+                else if (type == typeof(byte[]))
+                {
+                    var dataBytes = (byte[]) data;
+                    return IJsonConverterProvider.TextEncoding.GetString(dataBytes);
+                }
+                else if(type == typeof(string) || type == typeof(String))
+                    return data as string;
                 else
                     return data.ToString();
             }
